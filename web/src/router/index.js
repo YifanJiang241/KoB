6bf284e7bff20b5ce845fd22bfe4b306d526ba74
Jyf <1378749840@qq.com>
2023-04-07 16:32:16 +0800

    实现了导航栏及对战页面地图障碍物

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,8 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PKindexView from '@/views/pk/PKindexView'
+import RanklistindexView from '@/views/ranklist/RanklistindexView'
+import RecordlistindexView from '@/views/record/RecordlistindexView'
+import UserBotindexView from '@/views/user/bot/UserBotindexView'
+import NotFound from '@/views/error/NotFound'
 
 
 const routes = [
-  
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/"
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PKindexView,
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordlistindexView,
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RanklistindexView,
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotindexView,
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFound,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404/"
+  },
+
  
 ]
 
