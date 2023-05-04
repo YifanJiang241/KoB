2d49f46e7ebfac28a6e70c707794bdffa879deb5
Jyf <1378749840@qq.com>
2023-05-04 21:22:14 +0800

    6.1实现微服务：匹配系统（上）

--- web/src/views/pk/PKindexView.vue
+++ web/src/views/pk/PKindexView.vue
@@ -1,6 +1,6 @@
 <template>
-    <PlayGround />
-       
+    <PlayGround v-if="$store.state.pk.status === 'playing'"/>
+    <MatchGround v-if="$store.state.pk.status === 'matching'"/>
         
     
     
@@ -9,12 +9,64 @@
 
 <script>
 //import ContentField from "@/components/ContentField.vue"
-
+import { onMounted, onUnmounted } from 'vue';
 import PlayGround from'@/components/PlayGround.vue'
+import { useStore } from 'vuex';
+import MatchGround from '@/components/MatchGround.vue'
+
+
+
+
 export default{
     components: {
-        PlayGround
-    }
+        PlayGround,
+        MatchGround,
+    },
+    setup() {
+        const store = useStore();
+        
+        const socketUrl = `ws://localhost:3000/websocket/${store.state.user.token}/`
+
+        let socket = null;
+        onMounted(() => {
+
+            store.commit("updateOpponent",{
+                username: "我的对手",
+                photo: "https://cdn.acwing.com/media/article/image/2022/08/09/1_1db2488f17-anonymous.png"
+            })
+
+            socket = new WebSocket(socketUrl);
+
+            socket.onopen = () => {
+                console.log("connected!");
+                store.commit("updateSocket", socket);
+            }
+
+            socket.onmessage = msg => {
+                const data = JSON.parse(msg.data);
+                console.log(data);
+                if(data.event === "start-matching") {//匹配成功
+                    store.commit("updateOpponent", {
+                        username: data.opponent_username,
+                        photo: data.opponent_photo,
+                    });
+                    setTimeout(() => {
+                        store.commit("updateStatus", "playing");
+                    },2000);
+                    store.commit("updateGamemap", data.gamemap);
+                    
+                }
+            }
+
+            socket.onclose = () => {
+                console.log("disconnected!");
+            }
+        });
+        onUnmounted(() => {
+            socket.onclose();
+            store.commit("updateStatus", "matching");
+        })
+    } 
 }
 </script>
 
